93e97177e4434963eb2dadb8d274febd05b222c5
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-13 15:06:57 -0400

    H

--- public/actions.js
+++ public/actions.js
@@ -1,6 +1,6 @@
 (function worker(){
     var request = $.ajax({
-                    url: "http://localhost:5000/winner",
+                    url: "http://votation-up.herokuapp.com/winner",
                     dataType: "text"
                   });
                   request.done(function( msg ) {
@@ -12,10 +12,9 @@
                     setTimeout(worker, 2000);
                   })
 })()
-// https://votation-up.herokuapp.com/winner
+
 $("#si").click(function(){
-  ///$.get("https://votation-up.herokuapp.com/si", function( data, err ) {
-    $.get("http://localhost:5000/si", function( data, err ) {
+    $.get("http://votation-up.herokuapp.com/si", function( data, err ) {
     if(!err) throw  err
     $('#siV').html(data)
     console.log("Sí: "+data)
@@ -23,7 +22,7 @@ $("#si").click(function(){
 })
 
 $("#no").click(function(){
-  $.get("http://localhost:5000/no", function( data, err ) {
+  $.get("http://votation-up.herokuapp.com/no", function( data, err ) {
     if(!err) throw  err
     $('#noV').html(data)
     console.log("No: "+data)
@@ -31,7 +30,7 @@ $("#no").click(function(){
 })
 
 $("#noSe").click(function(){
-  $.get("http://localhost:5000/noSe", function( data, err ) {
+  $.get("http://votation-up.herokuapp.com/noSe", function( data, err ) {
     if(!err) throw  err
     $('#noSeV').html(' '+data)
     console.log("No Sé: "+data)
--- public/main.js
+++ public/main.js
@@ -1,9 +1,8 @@
 
 //var socket = io.connect('https://votation-up.herokuapp.com:6969', { 'forceNew': true , 'secure': true});
-var socket = io.connect('http://localhost:6969', { 'forceNew': true , 'secure': true});
+var socket = io.connect('http://votation-up.herokuapp.com:6969', { 'forceNew': true , 'secure': true});
 
 socket.on('messages', function(data) {
-
     console.log(data);
 
 });
