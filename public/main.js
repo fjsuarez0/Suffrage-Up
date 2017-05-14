8c3695f5939cd7bafc7fae2b498b6debf40b210e
Tron <fjsuarez@urbe.edu.ve>
2017-05-13 20:29:37 -0400

    Clean socket client

--- public/main.js
+++ public/main.js
@@ -1,7 +1,3 @@
-
-//
-vara PORT = {{socketPort}}
-//var socket = io.connect('http://votation-up.herokuapp.com:'+PORT, { 'forceNew': true , 'secure': true});
 var socket = io.connect('https://votation-up.herokuapp.com:6969', { 'forceNew': true , 'secure': true});
 socket.on('messages', function(data) {
     console.log(data);
