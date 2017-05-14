494cef46c98b131ca207ebdffcb51a3075d324a8
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-13 20:55:52 -0400

    Change in: package.json

---
+++ package.js
@@ -0,0 +1,26 @@
+{
+  "name": "clear",
+  "version": "1.0.0",
+  "description": "",
+  "main": "app.js",
+  "scripts": {
+    "start": "node-dev index.js",
+    "dev": "node-dev index.js",
+    "build": "console.log(´Hello ´)",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.15.2",
+    "express-handlebars": "^3.0.0",
+    "firebase": "^3.7.3",
+    "moment": "^2.18.1",
+    "moments": "0.0.2",
+    "mongoose": "^4.9.0",
+    "socket.io": "^1.7.3"
+  },
+  "engines": {
+    "node": "6.10.1"
+  }
+}
