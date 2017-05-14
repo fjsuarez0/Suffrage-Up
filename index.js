6fa31ff6eae8905dbc047c5e6167df2b2984900f
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-14 16:17:31 -0400

    a

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const path = require('path')
+
 const express = require('express')
+
 const app = express()
+
 const server = require('http').Server(app)
+
 const io = require('socket.io')(server)
+
 const exphbs = require('express-handlebars')
+
 const mongoose = require('mongoose')
-const components = require("./functions/components")
-const functions = require("./functions/functions")
-const models = require("./model/schema_model")
+
+const functions = require('./functions/functions')
+
+const models = require('./model/schema_model')
+
+//const moment = require('moment')
+
+const d = moment().format('dddd, MMMM Do YYYY, h:mm:ss a')
+
+//const admin = require('firebase-admin')
+
 
 const port = process.env.PORT || 9000
 // Middleswares Configuration
