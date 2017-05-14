81eca322e6ff4f549703f65cf142d124707349b0
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-13 21:46:07 -0400

    a

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://votation-up.herokuapp.com:6969', { 'forceNew': true , 'secure': true});
+var socket = io.connect('http://votation-up.herokuapp.com:6969', { 'forceNew': true , 'secure': true});
 socket.on('messages', function(data) {
     console.log(data);
 
