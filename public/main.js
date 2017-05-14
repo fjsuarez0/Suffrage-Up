71a1ff4cceb0c5713b231b2b9aba782540f41c4d
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-14 15:57:39 -0400

    w

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 
 // What happend ? 
-var socket = io.connect('https://votation-up.herokuapp.com', {'secure': true});
+var socket = io.connect('https://votation-up.herokuapp.com:6969', {'secure': true});
 socket.on('messages', function(data,err) {
     console.log(data);
+    console.log(err)
 });
