060aeffde97a8b3147c7dacc6809aec24a690026
Jhon Carter <fjsuarez@urbe.edu.ve>
2017-05-15 13:12:25 -0400

    Port

--- public/main.js
+++ public/main.js
@@ -1,7 +1,8 @@
 
 // What happend ?  asdsad
-//var socket = io.connect('https://votation-up.herokuapp.com:6969', {'secure': true});
-var socket = io()
+//var socket = io()
+// polling-xhr.js:261 GET https://votation-up.herokuapp.com/socket.io/?EIO=3&transport=polling&t=LmChkRt 404 (Not Found)
+var socket = io.connect('https://votation-up.herokuapp.com:6969', {'secure': true});
 socket.on('messages', function(data,err) {
     console.log(data);
     console.log(err)
